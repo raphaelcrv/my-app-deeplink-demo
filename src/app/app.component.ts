c54ac0262d32297fb74a99ea147c9aa988a805e0
Raphael Souza <raphael@devmagic.com.br>
2018-09-08 15:04:27 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,37 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Deeplinks} from '@ionic-native/deeplinks';
 
 import { HomePage } from '../pages/home/home';
+import { ProdutoPage } from '../pages/produto/produto';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) navChild: Nav;
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar, 
+    deeplinks: Deeplinks,
+    splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      deeplinks.routeWithNavController(this.navChild, {
+        'produto/:cod' : ProdutoPage,
+      }).subscribe((match)=> {
+        console.log('Native Route', match);
+      }), (nomatch) => {
+        console.log('didnt mache :( test');
+      }
+
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ProdutoPageModule } from '../pages/produto/produto.module';
+import { Deeplinks } from '@ionic-native/deeplinks';
+import { ProdutoPage } from '../pages/produto/produto';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,16 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    ProdutoPageModule,
+    IonicModule.forRoot(MyApp,
+      {
+        //locationStrategy: 'path',
+      },
+      {
+      links: [
+        {component: ProdutoPage, name : 'produto', segment: 'produto/:cod'},
+      ]
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +36,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    Deeplinks,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
   ]
 })
 export class AppModule {}
